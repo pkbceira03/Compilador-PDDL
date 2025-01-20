cf3f950372ff38919b099185aaa8064352af44fe
pkbceira03 <cabeceira2003@gmail.com>
2025-01-20 09:44:10 -0300

    adiciona função de remover comentario e adiciona arquivos de pddl para teste

--- lexico.py
+++ lexico.py
@@ -1,8 +1,10 @@
-#variaveis
-variaveis = r"\?[a-zA-Z][a-zA-Z0-9]*"
+import os
 
-#constantes
-constantes = r"\[a-zA-Z0-9]*"
+letra = r"\?[a-zA-Z]"
+
+digitos = r"\[0-9]*"
+
+letras_digitos = letra + digitos
 
 #lista de palavras reservadas
 #acho que o problem entra em outra categario pois não começa com o:
@@ -21,7 +23,76 @@ operadores = [
     'increse','imply'
 ]
 
-#simbolos que são usados
-simbolos = [
-    '(',')','-'
+#delimitador
+delimitador = [
+    '(',')'
 ]
+
+#simbolos
+simbolos = [
+    '-', ':',';'
+]
+
+#tokens
+DELEMITADOR = []
+SIMBOLO =[]
+OPRADORES = []
+PALAVRAS_RESERVADAS = []
+COMENTARIO = []
+VARIAVEL = []
+CONSTANTE = []
+FUNCAO = []
+
+#armazena todos os tokens
+token = []
+
+#função principal
+def lexico(codigo):
+    posicao = 0
+    
+    #remove os comentarios
+    def tira_comentario(posicao):
+        posicao_aux = posicao
+        comentario = []
+        #ir ate o fim da linha que é o \n
+        #adicionar no token 
+        #continuar o while passado na proxima linha 
+        comentario.append(codigo[posicao_aux])
+        posicao_aux += 1
+        while posicao_aux<len(codigo):
+            caracter = codigo[posicao_aux]
+            if caracter == '\n':
+                posicao_aux +=1
+                break
+            comentario.append(codigo[posicao_aux])
+            posicao_aux += 1
+
+        print(f"Conteudo do comentario -> {comentario}")
+        COMENTARIO.append(codigo[posicao:posicao_aux])
+        token.append(f"COMENTARIO = {COMENTARIO}")
+        posicao = posicao_aux
+
+        return posicao
+
+    #função para ver o proximo caracter
+    def proximo(posicao):
+        if posicao < len(codigo):
+            return codigo[posicao]
+        else:
+            return None
+
+    #percorre o arquivo inteiro  
+    while posicao < len(codigo):
+        if proximo(posicao) == ';':
+            posicao = tira_comentario(posicao)
+            continue
+        posicao += 1
+
+caminho = os.path.abspath("problem.pddl")
+print (caminho)
+
+with open('/home/pedro/unb/Compiladores/Trabalho/Compilador-PDDL/problem.pddl', mode='r', encoding='utf-8')as file:
+    codigo = file.read()    
+
+lexico(codigo)
+
