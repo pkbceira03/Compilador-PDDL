bd81363854bafedc5cff80ac00bf79170bc1bf83
pkbceira03 <cabeceira2003@gmail.com>
2025-02-05 14:47:56 -0300

    adiciona criação de todos os tokens, leitura do arquivo e exemplos de pddl do moj

--- lexico.py
+++ lexico.py
@@ -17,7 +17,7 @@ letra_digitos = letra + digitos
 palavras_reservadas = [
     "define", "domain", "requirements", "types", 
     "constants", "predicates", "functions", "constraints", 
-    "action" "parameters", "precondition", "effect", 
+    "action", "parameters", "precondition", "effect", 
     "durative-action", "duration", "condition", "derived", 
     "problem", "objects", "init", "goal", "metric", 
     "total-time", "lenght", "serial", "parallel"
@@ -60,33 +60,28 @@ otimizacao = [
 
 #delimitador
 delimitador = [
-    '(',')', ':'
+    '(',')'
 ]
-
-#simbolos
-simbolos = [
-    '-',';','?'
-]
-
-#Caracteres aceitos
-caracter = digitos + delimitador + simbolos
 
 #espaco
 espaco = [" ","\n","\t","\r"]
+
 #tokens
+#feitos
 DELEMITADOR = [] #delimiter
-PALAVRAS_RESERVADAS = [] #heyword
 COMENTARIO = [] #comment
-NUMERO = [] #number
 IDENTIFICADOR = [] #identifier
 VARIAVEL = [] #varialvel ? + identificador
+PALAVRAS_RESERVADAS = [] #heyword
 OPERADORES_LOGICO = [] #logical operator
-OPERADORES_ARITMETICO = [] #arithmetic logical 
 OPERADORES_QUANTIFICADORES = [] #quantifier operator
 OPERADORES_CONDICIONAIS = [] #conditional operator
 OPERADORES_DE_MODIFICACAO = [] #modifier operator
 OPERADORES_TEMPORAIS = [] #temporal operador 
 OPERADORES_DE_OTIMIZACAO = [] #optimization operator
+NUMERO = [] #number
+OPERADORES_ARITMETICO = [] #arithmetic logical 
+NAO_RECONHECE = [] #unknow
 
 
 #armazena todos os tokens
@@ -95,30 +90,6 @@ token = []
 #função principal
 def lexico(codigo):
     posicao = 0
-    
-    #remove os comentarios
-    def tira_comentario(posicao):
-        posicao_aux = posicao
-        comentario = []
-        #ir ate o fim da linha que é o \n
-        #adicionar no token 
-        #continuar o while passado na proxima linha 
-        comentario.append(codigo[posicao_aux])
-        posicao_aux += 1
-        while posicao_aux<len(codigo):
-            caracter = codigo[posicao_aux]
-            if caracter == '\n':
-                posicao_aux +=1
-                break
-            comentario.append(codigo[posicao_aux])
-            posicao_aux += 1
-
-        #print(f"Conteudo do comentario -> {comentario}")
-        COMENTARIO.append(codigo[posicao:posicao_aux])
-        token.append(f"COMENTARIO = {COMENTARIO}")
-        posicao = posicao_aux
-
-        return posicao
 
     #função para ver o proximo caracter
     def proximo(posicao):
@@ -138,39 +109,187 @@ def lexico(codigo):
                 linha += 1
             posicao += 1
 
-        #tira comentarios
+        #encontra comentarios
         if proximo(posicao) == ';':
-            posicao = tira_comentario(posicao)
+            posicao_aux = posicao
+            comentario = []
+            #ir ate o fim da linha que é o \n
+            #adicionar no token 
+            #continuar o while passado na proxima linha 
+            comentario.append(codigo[posicao_aux])
+            posicao_aux += 1
+            while posicao_aux<len(codigo):
+                caracter = codigo[posicao_aux]
+                if caracter == '\n':
+                    posicao_aux +=1
+                    break
+                comentario.append(codigo[posicao_aux])
+                posicao_aux += 1
+
+            #print(f"Conteudo do comentario -> {comentario}")
+            COMENTARIO.append(codigo[posicao:posicao_aux])
+            token.append(f"COMENTARIO = {comentario}")
+            posicao = posicao_aux
+        elif proximo(posicao) == ':':
+            DELEMITADOR.append(proximo(posicao))
+            token.append(f"DELIMITATOR = {proximo(posicao)}")
             posicao += 1
-            continue
+            while proximo(posicao) is not None and proximo(posicao) not in espaco and (proximo(posicao) in letra_digitos or proximo(posicao) == '-'):
+                palavra += proximo(posicao)
+                posicao += 1
+            PALAVRAS_RESERVADAS.append(palavra)
+            token.append(f"PALAVRA_RESERVADA = {palavra}")
+            palavra =''
+            
         elif proximo(posicao) in delimitador:
             #encontra os delimitadores
             DELEMITADOR.append(proximo(posicao))
             token.append(f"DELIMITATOR = {proximo(posicao)}")
             #print(proximo(posicao))
             posicao += 1
+        #encontra as variaveis
+        elif proximo(posicao) == '?':
+            aux_variavel = ''
+            aux_variavel += proximo(posicao)
+            posicao += 1
+            if proximo(posicao) in digitos:
+                palavra += proximo(posicao-1)
+                while proximo(posicao) not in espaco:
+                    palavra += proximo(posicao)
+                    aux_variavel += proximo(posicao)
+                    posicao += 1
+                NAO_RECONHECE.append(palavra)
+                token.append(f"NAO_RECONHECE = {palavra}")
+                posicao += 1
+                palavra = ''
+            else:
+                while proximo(posicao) not in espaco and proximo(posicao) in letra_digitos:
+                    palavra += proximo(posicao)
+                    aux_variavel += proximo(posicao)
+                    posicao += 1
+
+                IDENTIFICADOR.append(palavra)
+                VARIAVEL.append(aux_variavel)
+                token.append(f"IDENTIFICADOR = {palavra}")
+                token.append(f"VARIAVEL = {aux_variavel}")
+                palavra = ''
+                aux_variavel = ''
+                posicao += 1
+            
+        elif proximo(posicao) in letra:
+            palavra += proximo(posicao)
+            posicao+=1
+            while proximo(posicao) is not None and proximo(posicao) not in espaco and (proximo(posicao) in letra_digitos or proximo(posicao) == '-'):
+                palavra += proximo(posicao)
+                posicao += 1
+            
+            if palavra in palavras_reservadas:
+                PALAVRAS_RESERVADAS.append(palavra)
+                token.append(f"PALAVRA_RESERVADA = {palavra}")
+            elif palavra in logicos:
+                OPERADORES_LOGICO.append(palavra)
+                token.append(f"OPERADOR_LOGICO = {palavra}")
+            elif palavra in quantificador:
+                OPERADORES_QUANTIFICADORES.append(palavra)
+                token.append(f"OPERADOR QUATIFICADOR = {palavra}")
+            elif palavra in condicional:
+                OPERADORES_CONDICIONAIS.append(palavra)
+                token.append(f"OPERADOR CONDICIONAL = {palavra}")
+            elif palavra in temporal:
+                OPERADORES_TEMPORAIS.append(palavra)
+                token.append(f"OPERADOR TEMPORAL = {palavra}")
+            elif palavra in otimizacao:
+                OPERADORES_DE_OTIMIZACAO.append(palavra)
+                token.append(f"OPERADOR DE OTIMIZACAO = {palavra}")
+            elif palavra in modificador:
+                OPERADORES_DE_MODIFICACAO.append(palavra)
+                token.append(f"OPERADOR DE MODIFICACAO = {palavra}")
+            else:
+                IDENTIFICADOR.append(palavra)
+                token.append(f"IDENTIFICADOR = {palavra}")
+            
+            palavra=''
+        elif proximo(posicao) in aritmeticas:
+            OPERADORES_ARITMETICO.append(proximo(posicao))
+            token.append(f"OPERADOR ARITMETICO = {proximo(posicao)}")
+            posicao +=1
+        #pega os numeros
+        elif proximo(posicao) in digitos:
+            palavra += proximo(posicao)
+            posicao += 1
+            while proximo(posicao) not in espaco:
+                conta=0
+                palavra += proximo(posicao)
+                if proximo(posicao) in letra:
+                    conta +=1
+            if conta > 0:
+                NAO_RECONHECE.append(palavra)
+                token.append(f"NAO_RECONHECE = {palavra}")
+                
+            else:
+                NUMERO.append(palavra)
+                token.append(f"NUMERO = {palavra}")
+            palavra = ''
+            
+
         else:
-            # print('pula')
-            # print(proximo(posicao))
+            #print('pula')
+            #print(proximo(posicao))
+            if proximo(posicao):
+                NAO_RECONHECE.append(proximo(posicao))
+                token.append(f"NAO_RECONHECE = {proximo(posicao)}")
             posicao+=1
                 
-        
+import sys
+import os
 
-caminho = os.path.abspath("problem.pddl")
-print (caminho)
+if len(sys.argv) < 3:
+    sys.exit(1)
 
-with open('/home/pedro/unb/Compiladores/Trabalho/Compilador-PDDL/domain.pddl', mode='r', encoding='utf-8')as file:
-    codigo = file.read()    
+arq1 = sys.argv[1]
+arq2 = sys.argv[2]
 
-lexico(codigo)
+with open(arq1, mode='r', encoding='utf-8') as file:
+    codigo1 = file.read()
 
-print('comentario')
-print(len(COMENTARIO))
-print(COMENTARIO)
-print('delimitador')
-print(len(DELEMITADOR))
+with open(arq2, mode='r', encoding='utf-8') as file:
+    codigo2 = file.read()    
 
+lexico(codigo1)
+lexico(codigo2)
 
+###como rodar 
+##============================================================##
+##python3 lexico.py exemplomojproblem.pddl exemplomojdomain.pddl
+
+print('KEYWORD: ',len(PALAVRAS_RESERVADAS))
+# print(PALAVRAS_RESERVADAS)
+print('IDENTIFIER: ',len(IDENTIFICADOR))
+#print(IDENTIFICADOR)
+print('VARIABLES: ',len(VARIAVEL))
+#print(VARIAVEL)
+print('NUMBER: ',len(NUMERO))
+#print(NUMERO)
+print('ARITHMETIC_OPERATOR: ', len(OPERADORES_ARITMETICO))
+# print(OPERADORES_ARITMETICO)
+print('LOGICAL_OPERATOR: ',len(OPERADORES_LOGICO))
+#print(OPERADORES_LOGICO)
+print('QUANTIFIER_OPERATOR: ',len(OPERADORES_QUANTIFICADORES))
+#print(OPERADORES_QUANTIFICADORES)
+print('CONDITIONAL_OPERATOR: ',len(OPERADORES_CONDICIONAIS))
+#print(OPERADORES_CONDICIONAIS)
+print('MODIFIER_OPERATOR: ',len(OPERADORES_DE_MODIFICACAO))
+#print(OPERADORES_DE_MODIFICACAO)
+print('TEMPORAL_OPERATOR: ',len(OPERADORES_TEMPORAIS))
+#print(OPERADORES_TEMPORAIS)
+print('OPTIMIZATION_OPERATOR: ',len(OPERADORES_DE_OTIMIZACAO))
+#print(OPERADORES_DE_OTIMIZACAO)
+print('DELIMITER: ',len(DELEMITADOR))
+# print(DELEMITADOR)
+print('COMMENTS: ',len(COMENTARIO))
+#print(COMENTARIO)
+print('UNKNOWN: ',len(NAO_RECONHECE))
+#print(NAO_RECONHECE)
 
 # print('token')
 # print(token)
